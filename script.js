c242eb091e4877078c705e472bf8204ab9ed7e80
Tamm <154392330+ISharvn@users.noreply.github.com>
2023-12-24 15:48:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let hrBox = document.getElementById("hr-box");
 let minBox = document.getElementById("min-box");
 let secBox = document.getElementById("sec-box");
 let endDate = new Date(2024, 0, 1, 00, 00);
-let endTime = endDate.getTime();
+let endTime = endDate.getTime(2023, 7, 1, 00, 00);
 
 function countdown() {
   let todayDate = new Date();
